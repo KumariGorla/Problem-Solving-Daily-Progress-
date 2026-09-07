8a3c68317cc8f7c66f9622eb30e13e7ecddbc154
kumarigorla <kumarigorla09@gmail.com>
2026-09-07 16:20:11 +0530

    Added new topic

---
+++ enumpr.java
@@ -0,0 +1,10 @@
+enum Status{
+    Running, Failed, Success;
+}
+public class enumpr {
+    public static void main(String args[]){
+        
+        Status s = Status.Running;
+        System.out.println(s);
+    }
+}
